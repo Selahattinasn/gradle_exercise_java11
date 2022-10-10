78d06c1d335b3a0dd24d1e808c1646c656fd42bf
SelahattinKarakaya <selahattin.karakaya@nexus-ips.de>
2022-10-10 11:34:52 +0200

    for java11

---
+++ src/test/java/net/nxips/containertest/ContainertestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nxips.containertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
